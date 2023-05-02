5f55582b5a178f2ded69c9a7ad7841b5e1078188
Shdrwith <128394535+Shdrwith@users.noreply.github.com>
2023-05-02 21:02:27 +0100

    CW3 3.3
    def initial_board(board):
        for x in range(9):
            for y in range(9):
                print(board[x][y], end=' ')
            print()
    
    def find_empty(board):
        for x in range(9):
            for y in range(9):
                if board[x][y] == 0:
                    return (x,y)
        return None
    
    def is_valid(board, row, col, num):
        for y in range(9):
            if board[row][y] == num:
                return False
        for x in range(9):
            if board[x][col] == num:
                return False
        square_row = (row // 3) * 3
        square_col = (col // 3) * 3 
        for x in range(square_row, square_row + 3):
            for y in range(square_col, square_col + 3): 
                if board[x][y] == num:
                    return False
        return True
    
    def solve_board(board):
        empty_part = find_empty(board)
        if not empty_part:
            return True
        row, col = empty_part
        for num in range(1,10):
            if is_valid(board, row, col, num):
                board[row][col]=num
                if solve_board(board):
                    return True
                board[row][col]=0
        return False
    
    board = [
        [9, 0, 6, 0, 0, 1, 0, 4, 0],
        [7, 0, 1, 2, 9, 0, 0, 6, 0],
        [4, 0, 2, 8, 0, 6, 3, 0, 0],
        [0, 0, 0, 0, 2, 0, 9, 8, 0],
        [6, 0, 0, 0, 0, 0, 0, 0, 2],
        [0, 9, 4, 0, 8, 0, 0, 0, 0],
        [0, 0, 3, 7, 0, 8, 4, 0, 9],
        [0, 4, 0, 0, 1, 3, 7, 0, 6],
        [0, 6, 0, 9, 0, 0, 1, 0, 8]
        ]
    
    print("begging_board:")
    initial_board(board)
    
    solve_board(board)
    print("\n")
    
    print("end_board:")
    initial_board(board)

---
+++ CW3 3.3.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 25 17:32:31 2023
+
+@author: zyx34
+"""
+
+def initial_board(board):
+    for x in range(9):
+        for y in range(9):
+            print(board[x][y], end=' ')
+            if y == 2 or y == 5:
+                print(' ', end=' ')
+        print()
+        if x == 2 or x == 5:
+            print(' '*21)
+
+def find_empty(board):
+    for x in range(9):
+        for y in range(9):
+            if board[x][y] == 0:
+                return (x,y)
+    return None
+
+def is_valid(board, row, col, num):
+    for y in range(9):
+        if board[row][y] == num:
+            return False
+    for x in range(9):
+        if board[x][col] == num:
+            return False
+    square_row = (row // 3) * 3
+    square_col = (col // 3) * 3 
+    for x in range(square_row, square_row + 3):
+        for y in range(square_col, square_col + 3): 
+            if board[x][y] == num:
+                return False
+    return True
+
+def solve_board(board):
+    empty_part = find_empty(board)
+    if not empty_part:
+        return True
+    row, col = empty_part
+    for num in range(1,10):
+        if is_valid(board, row, col, num):
+            board[row][col]=num
+            if solve_board(board):
+                return True
+            board[row][col]=0
+    return False
+
+board = [
+    [9, 0, 6, 0, 0, 1, 0, 4, 0],
+    [7, 0, 1, 2, 9, 0, 0, 6, 0],
+    [4, 0, 2, 8, 0, 6, 3, 0, 0],
+    [0, 0, 0, 0, 2, 0, 9, 8, 0],
+    [6, 0, 0, 0, 0, 0, 0, 0, 2],
+    [0, 9, 4, 0, 8, 0, 0, 0, 0],
+    [0, 0, 3, 7, 0, 8, 4, 0, 9],
+    [0, 4, 0, 0, 1, 3, 7, 0, 6],
+    [0, 6, 0, 9, 0, 0, 1, 0, 8]
+    ]
+
+print("begging_board:")
+initial_board(board)
+
+solve_board(board)
+print("\n")
+
+print("end_board:")
+initial_board(board)
